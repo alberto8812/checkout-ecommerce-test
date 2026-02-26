63904d4c22a3c0c31af88053e404060fe31e762f
Carlos8992 <cvelasco@saludelectronica.com>
2026-02-26 18:19:31 -0500

    feat: Add Swagger API documentation, global validation pipes, and enhanced CORS configuration.

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,9 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { Logger, ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
+  const logger = new Logger('checkout-ecommerce');
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  // Habilitar CORS
+  app.enableCors({
+    origin: '*', // O pon aquí el origen de tu frontend
+    credentials: true,
+  });
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+  const config = new DocumentBuilder()
+    .setTitle('Checkout Ecomerce')
+    .setDescription('Documentación de la API de Checkout Ecomerce')
+    .setVersion('1.0')
+    .addTag('checkout-ecommerce')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(process.env.PORT ?? 3000);
+  logger.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
