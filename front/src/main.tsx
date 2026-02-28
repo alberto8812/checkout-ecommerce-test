beb2b003b4759877ea51ddb67e735710854db41e
Carlos8992 <cvelasco@saludelectronica.com>
2026-02-28 11:01:02 -0500

    chore: integrate React Query Devtools for development.

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { TooltipProvider } from '@/components/ui/tooltip'
 import './index.css'
 import App from './App.tsx'
@@ -13,6 +14,7 @@ createRoot(document.getElementById('root')!).render(
       <TooltipProvider>
         <App />
       </TooltipProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </StrictMode>,
 )
