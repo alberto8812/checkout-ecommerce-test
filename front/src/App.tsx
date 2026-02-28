69202911a395effb24fe86f9d4f014a198360d2a
Carlos8992 <cvelasco@saludelectronica.com>
2026-02-27 21:49:38 -0500

    feat: integrate Redux for state management
    - Wrapped the RouterProvider with Redux Provider in App.tsx to enable global state management.
    - Created a new Redux store in redux.global.store.ts with a placeholder for future reducers.
    - Added necessary dependencies for Redux Toolkit and React-Redux in package.json.

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,9 +1,13 @@
 import { RouterProvider } from "react-router-dom";
-
+import { router } from "./shared/router/router";
+import { Provider } from "react-redux";
+import { store } from "./shared/stores/redux.global.store";
 function App() {
   return (
     <>
-      <RouterProvider router={router} />
+      <Provider store={store}>
+        <RouterProvider router={router} />
+      </Provider>
     </>
   );
 }
